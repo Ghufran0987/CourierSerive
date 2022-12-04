e00e3f4d356ff41cf0da3d1043efff7b71499ece
Ghufran0987 <ghufranm054@gmail.com>
2022-12-04 07:57:32 -0800

    added database and database context

---
+++ CourierSerive/Context/CourierDbContext.cs
@@ -0,0 +1,19 @@
+﻿using CourierSerive.Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace CourierSerive.Context
+{
+    public class CourierDbContext:DbContext
+    {
+        public CourierDbContext():base("name=CourierDbContext") 
+        { 
+
+        }
+
+        public DbSet<Booking> Booking { get; set; }
+    }
+}
---
+++ CourierSerive/Models/Entities/Booking.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace CourierSerive.Models.Entities
+{
+    public class Booking
+    {
+        [Key]
+
+        public Guid BookingId { get; set; }
+        [Display(Name ="Booking Name")]
+        [Required(ErrorMessage = "Please Booking Name")]
+        public string BookingName { get; set; }
+        [Required(ErrorMessage = "Please Enter Amount")]
+        public int Amount { get; set; }
+
+        [Display(Name = "Contact No")]
+        [Required(ErrorMessage = "Please Enter Contact No")]
+        public string ContactNo { get; set; }
+
+        [Required(ErrorMessage ="Please Enter Address")]
+        public string Address { get; set; }
+
+        [Display(Name = "User Name")]
+        public string UserName { get; set; }
+        public string UserId { get; set; }
+        public DateTime CreatedDate { get; set; }
+    }
+}
